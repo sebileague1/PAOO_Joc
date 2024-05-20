a083321732a25c74e237d421dec9cf3176dc15f3
Danalache Sebastian <72085251+sebileague1@users.noreply.github.com>
2024-05-20 16:33:37 +0300

    Delete Tanks_Game directory

--- Tanks_Game/src/Main.java
+++
@@ -1,9 +0,0 @@
-import Game.*;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Game game = new Game("TANKS GAME", 800, 600);
-        game.start();
-    }
-}
--- Tanks_Game/src/UI/ClickListener.java
+++
@@ -1,6 +0,0 @@
-package UI;
-
-public interface ClickListener {
-    void onClick();
-
-}
